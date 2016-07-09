a2acbc9d988ac3ae780b8e115b7683d3d3ea82f6
Federico Tomassetti <federico@tomassetti.me>
2016-07-09 21:28:32 +0200

    initial kotlin ParseTreeToAstMapper

---
+++ src/main/java/me/tomassetti/antlrplus/ast/ParseTreeToAstMapper.kt
@@ -0,0 +1,236 @@
+package me.tomassetti.ast
+
+import org.antlr.v4.Tool
+import org.antlr.v4.tool.*
+import org.antlr.v4.tool.ast.AltAST
+import org.antlr.v4.tool.ast.GrammarAST
+import org.antlr.v4.tool.ast.RuleRefAST
+import org.antlr.v4.tool.ast.TerminalAST
+import java.util.*
+
+data class Element(val name: String, val type: String, val multiple: Boolean, val toExclude: List<String> = LinkedList<String>())
+
+class Entity(val name: String, val elements: List<Element>, val superclass: Entity? = null, val isAbstract: Boolean = false) {
+    override fun toString(): String{
+        return "Entity(name='$name', elements=$elements, superclass=${superclass?.name}, isAbstract=$isAbstract)"
+    }
+
+    override fun equals(other: Any?): Boolean{
+        if (this === other) return true
+        if (other !is Entity) return false
+
+        if (name != other.name) return false
+        if (elements != other.elements) return false
+        if (superclass != other.superclass) return false
+        if (isAbstract != other.isAbstract) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int{
+        var result = name.hashCode()
+        result = 31 * result + elements.hashCode()
+        result = 31 * result + (superclass?.hashCode() ?: 0)
+        result = 31 * result + isAbstract.hashCode()
+        return result
+    }
+}
+
+class Metamodel(val entities : List<Entity>) {
+    fun byName(name: String) : Entity {
+        val e = entities.find { e -> e.name == name }
+        if (e == null) {
+            throw IllegalArgumentException("Unknown entity $name")
+        } else {
+            return e
+        }
+    }
+}
+
+class ParseTreeToAstMapper() {
+
+    private val TOKEN_TYPE = "<String>"
+
+    private fun considerAlternative(alt : Alternative) : List<Element>{
+        val res = LinkedList<Element>()
+        alt.labelDefs.forEach { s, mutableList ->
+
+        }
+        val labelledTypes : java.util.HashMap<String, MutableList<String>> = HashMap<String, MutableList<String>>()
+        alt.labelDefs.forEach { s, mutableList ->
+            mutableList.forEach { e ->
+                processSingleLabel(e, labelledTypes, res)
+            }
+        }
+        alt.ruleRefs.forEach { s, mutableList ->
+            val labels = labelledTypes.getOrDefault(s, LinkedList<String>())
+            // Either all the elements have labels or none of them should have labels
+            // If they have labels we ignore them here
+            if (labels.size > 0 && mutableList.size != labels.size) {
+                res.add(Element(s, s, mutableList.size > 1, toExclude = labels))
+            } else if (labels.size == 0) {
+                res.add(Element(s, s, mutableList.size > 1))
+            }
+        }
+        alt.tokenRefs.forEach { s, mutableList ->
+            val nLabels = labelledTypes.getOrDefault(s, LinkedList<String>()).size
+            // Either all the elements have labels or none of them should have labels
+            // If they have labels we ignore them here
+            if (nLabels > 0 && mutableList.size != nLabels) {
+                throw UnsupportedOperationException()
+            } else if (nLabels == 0) {
+                res.add(Element(s, TOKEN_TYPE, mutableList.size > 1))
+            }
+        }
+        return res
+    }
+
+    private fun processSingleLabel(e: LabelElementPair, labelledTypes: java.util.HashMap<String, MutableList<String>>, res: LinkedList<Element>) {
+        when (e.type) {
+            LabelType.RULE_LIST_LABEL -> {
+                val type = (e.element as RuleRefAST).text
+                val name = (e.label as GrammarAST).text
+                if (!(labelledTypes.containsKey(type))) {
+                    labelledTypes.put(type, LinkedList<String>())
+                }
+                labelledTypes.get(type)?.add(name)
+                res.add(Element(name, type, true))
+            }
+            LabelType.RULE_LABEL -> {
+                val type = (e.element as RuleRefAST).text
+                val name = (e.label as GrammarAST).text
+                if (!(labelledTypes.containsKey(type))) {
+                    labelledTypes.put(type, LinkedList<String>())
+                }
+                labelledTypes.get(type)?.add(name)
+                res.add(Element(name, type, false))
+            }
+            LabelType.TOKEN_LABEL -> {
+                val type = (e.element as GrammarAST).text
+                val name = (e.label as GrammarAST).text
+                if (!(labelledTypes.containsKey(type))) {
+                    labelledTypes.put(type, LinkedList<String>())
+                }
+                labelledTypes.get(type)?.add(name)
+                res.add(Element(name, TOKEN_TYPE, false))
+            }
+            else -> throw UnsupportedOperationException("Label type ${e.type}")
+        }
+    }
+
+    fun produceMetamodel(antlrGrammarCode: String) : Metamodel {
+        val tool = Tool()
+        val root = tool.parseGrammarFromString(antlrGrammarCode)
+        val grammar = tool.createGrammar(root)
+        tool.removeListeners()
+        tool.addListener(object : ANTLRToolListener {
+            override fun warning(msg: ANTLRMessage?) {
+            }
+
+            override fun info(msg: String?) {
+            }
+
+            override fun error(msg: ANTLRMessage?) {
+            }
+        })
+        tool.process(grammar, false)
+
+        val entities = LinkedList<Entity>()
+
+        grammar.rules.forEach { s, rule ->
+            try {
+                if (rule is LeftRecursiveRule) {
+                    val superclass = Entity(s, LinkedList<Element>(), isAbstract = true)
+                    entities.add(superclass)
+                    if (rule.altLabels != null) {
+                        rule.altLabels.forEach { altLabel ->
+                            val name = altLabel.key
+                            val altAlts = altLabel.value.map { el -> el.b }
+                            processAltAsts(altAlts, entities, name, externalSuperclass = superclass)
+                        }
+                    }
+                } else {
+                    processRule(entities, rule, s)
+                }
+            } catch (e: RuntimeException) {
+                throw RuntimeException("Error processing rule $s", e)
+            }
+        }
+        return Metamodel(entities)
+    }
+
+    private fun processRule(entities: LinkedList<Entity>, rule: Rule, s: String) {
+        processAlternatives(rule.alt.toList(), entities, s)
+    }
+
+    private fun processAlternatives(alternativesRaw: Collection<Alternative>, entities: LinkedList<Entity>, s: String) {
+        val elements: MutableList<Element> = LinkedList<Element>()
+        val alternatives = alternativesRaw.filter { alt -> alt != null }
+        if (alternatives.size == 0) {
+            throw IllegalArgumentException("No alternatives for $s")
+        }
+        if (alternatives[0].ast.altLabel != null) {
+            // this is abstract
+            val superclass = Entity(s, elements, isAbstract = true)
+            entities.add(superclass)
+            alternatives.forEach { alt ->
+                entities.add(Entity(alt.ast.altLabel.text, considerAlternative(alt), superclass = superclass))
+            }
+        } else {
+            alternatives.forEach { alt -> elements.addAll(considerAlternative(alt)) }
+            entities.add(Entity(s, elements))
+        }
+    }
+
+    private fun isToken(grammarAST: GrammarAST) : Boolean {
+        return grammarAST.children.find { c -> !(c is TerminalAST) } == null
+    }
+
+    private fun processAltAsts(altAsts: Collection<AltAST>, entities: LinkedList<Entity>, s: String, externalSuperclass: Entity?) {
+        val elements: MutableList<Element> = LinkedList<Element>()
+        if (altAsts.size == 0) {
+            throw IllegalArgumentException("No alternatives for $s")
+        }
+        altAsts.forEach { altAst ->
+            altAst.children.forEach { el ->
+                when (el) {
+                    is TerminalAST -> elements.add(Element(el.token.text, TOKEN_TYPE, false))
+                    is RuleRefAST -> elements.add(Element(el.text, el.text, false))
+                    is GrammarAST -> if (el.childCount !=2) {
+                        throw UnsupportedOperationException()
+                    } else {
+                        val name = (el.children[0] as GrammarAST).text
+                        val subEl = el.children[1]
+                        when (subEl) {
+                            is RuleRefAST -> elements.add(Element(name, el.text, false))
+                            is GrammarAST -> if (isToken(subEl)) {
+                                elements.add(Element(name, TOKEN_TYPE, false))
+                            } else {
+                                throw UnsupportedOperationException()
+                            }
+                            else -> throw UnsupportedOperationException()
+                        }
+                    }
+                    else -> throw UnsupportedOperationException()
+                }
+            }
+        }
+        // remove duplicates
+        val elementNames = elements.map { e -> e.name }
+        elementNames.forEach { name ->
+            val elementsToMerge = elements.filter { e -> e.name == name }
+            if (elementsToMerge.count() != 1) {
+                // all must have the same type
+                elementsToMerge.forEach { e -> if (elementsToMerge[0].type != e.type) {
+                    throw UnsupportedOperationException()
+                } }
+                val multiple = elementsToMerge.find{ e -> e.multiple} != null
+                elements.removeAll(elementsToMerge)
+                elements.add(Element(name, elementsToMerge[0].type, multiple))
+            }
+        }
+        val entity = Entity(s, elements, superclass=externalSuperclass)
+        entities.add(entity)
+    }
+
+}
---
+++ src/test/java/me/tomassetti/antlrplus/ast/ParseTreeToAstMapperTest.kt
@@ -0,0 +1,46 @@
+package me.tomassetti.ast
+
+import kotlin.collections.*
+
+import org.antlr.v4.Tool
+import org.antlr.v4.parse.ANTLRParser
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.tool.ANTLRMessage
+import org.antlr.v4.tool.ANTLRToolListener
+import org.antlr.v4.tool.Alternative
+import org.antlr.v4.tool.LabelType
+import org.antlr.v4.tool.ast.*
+import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.StringReader
+import java.util.*
+import kotlin.test.assertEquals
+
+class ParseTreeToAstMapperTest {
+
+    fun convert(inputStream: InputStream) : String {
+        val result = ByteArrayOutputStream()
+        val buffer = ByteArray(1024)
+        var length: Int
+        do {
+            length = inputStream.read(buffer)
+            if (length != -1) {
+                result.write(buffer, 0, length)
+            }
+        } while (length != -1)
+        return result.toString("UTF-8")
+    }
+
+    @Test fun pythonMetamodel() {
+        val code = convert(this.javaClass.getResourceAsStream("/me/tomassetti/antlrplus/python/Python3.g4"))
+        val metamodel = ParseTreeToAstMapper().produceMetamodel(code)
+    }
+
+    @Test fun javaMetamodel() {
+        val code = convert(this.javaClass.getResourceAsStream("/me/tomassetti/antlrplus/java/Java8.g4"))
+        val metamodel = ParseTreeToAstMapper().produceMetamodel(code)
+    }
+
+}
