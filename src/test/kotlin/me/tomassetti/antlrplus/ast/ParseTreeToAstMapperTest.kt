35310d77bb35d4025cce5ff94bae8a1c621061a6
Federico Tomassetti <federico@tomassetti.me>
2016-07-10 10:40:50 +0200

    test and correct parsetree -> ast mapper

--- src/test/kotlin/me/tomassetti/antlrplus/ast/ParseTreeToAstMapperTest.kt
+++ src/test/kotlin/me/tomassetti/antlrplus/ast/ParseTreeToAstMapperTest.kt
@@ -37,12 +37,96 @@ class ParseTreeToAstMapperTest {
         val code = convert(this.javaClass.getResourceAsStream("/me/tomassetti/antlrplus/python/Python3.g4"))
         val metamodel = ParseTreeToAstMapper().produceMetamodel(code)
 
-        assertEquals(Entity("single_input", setOf(simpleToken("NEWLINE"), simpleChild("simple_stmt"), simpleChild("compound_stmt"))), metamodel.byName("single_input"))
+        assertEquals(
+                Entity("single_input", setOf(
+                    simpleToken("NEWLINE"),
+                    simpleChild("simple_stmt"),
+                    simpleChild("compound_stmt"))),
+                metamodel.byName("single_input"))
+
+        assertEquals(
+                Entity("file_input", setOf(
+                        multipleToken("NEWLINE"),
+                        simpleToken("EOF"),
+                        multipleChild("stmt"))),
+                metamodel.byName("file_input"))
+
+        assertEquals(
+                Entity("eval_input", setOf(
+                        multipleToken("NEWLINE"),
+                        simpleToken("EOF"),
+                        simpleChild("testlist"))),
+                metamodel.byName("eval_input"))
+
+        assertEquals(
+                Entity("decorator", setOf(
+                        simpleToken("NEWLINE"),
+                        simpleChild("dotted_name"),
+                        simpleChild("arglist"))),
+                metamodel.byName("decorator"))
+
+        assertEquals(
+                Entity("decorators", setOf(
+                        multipleChild("decorator"))),
+                metamodel.byName("decorators"))
+
+        assertEquals(
+                Entity("decorated", setOf(
+                        simpleChild("classdef"),
+                        simpleChild("funcdef"),
+                        simpleChild("decorators"))),
+                metamodel.byName("decorated"))
+
+        assertEquals(
+                Entity("funcdef", setOf(
+                        simpleToken("DEF"),
+                        simpleToken("NAME"),
+                        simpleChild("parameters"),
+                        simpleChild("test"),
+                        simpleChild("suite"))),
+                metamodel.byName("funcdef"))
+
+        assertEquals(
+                Entity("expr_stmt", setOf(
+                        multipleChild("testlist_star_expr"),
+                        simpleChild("augassign"),
+                        multipleChild("yield_expr"),
+                        simpleChild("testlist"))),
+                metamodel.byName("expr_stmt"))
+
+        assertEquals(
+                Entity("testlist_star_expr", setOf(
+                        multipleChild("test"),
+                        multipleChild("star_expr"))),
+                metamodel.byName("testlist_star_expr"))
+
     }
 
     @Test fun javaMetamodel() {
         val code = convert(this.javaClass.getResourceAsStream("/me/tomassetti/antlrplus/java/Java8.g4"))
         val metamodel = ParseTreeToAstMapper().produceMetamodel(code)
+
+        assertEquals(
+                Entity("classType", setOf(
+                        multipleChild("annotation"),
+                        simpleToken("Identifier"),
+                        simpleChild("typeArguments"),
+                        simpleChild("classOrInterfaceType"))),
+                metamodel.byName("classType"))
+
+        assertEquals(
+                Entity("classType_lf_classOrInterfaceType", setOf(
+                        multipleChild("annotation"),
+                        simpleToken("Identifier"),
+                        simpleChild("typeArguments"))),
+                metamodel.byName("classType_lf_classOrInterfaceType"))
+
+        assertEquals(
+                Entity("classType_lfno_classOrInterfaceType", setOf(
+                        multipleChild("annotation"),
+                        simpleToken("Identifier"),
+                        simpleChild("typeArguments"))),
+                metamodel.byName("classType_lfno_classOrInterfaceType"))
     }
 
 }
