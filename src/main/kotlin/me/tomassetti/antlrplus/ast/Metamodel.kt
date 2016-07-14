3ace1ce8b7fae430d9e6da7dbe77fdd8b402885d
Federico Tomassetti <federico@tomassetti.me>
2016-07-14 17:10:12 +0200

    creating the mapper

--- src/main/kotlin/me/tomassetti/antlrplus/ast/Metamodel.kt
+++ src/main/kotlin/me/tomassetti/antlrplus/ast/Metamodel.kt
@@ -1,6 +1,6 @@
 package me.tomassetti.antlrplus.ast
 
-import me.tomassetti.antlrplus.parsetree.PtEntity
+import java.util.*
 
 enum class Datatype {
     STRING,
@@ -19,16 +19,27 @@ data class Property(val name: String, val type: Datatype, val multiple: Boolean)
     override fun isMultiple() = multiple
 }
 
-data class Containment(val name: String, val type: PtEntity, val multiple: Boolean) : me.tomassetti.antlrplus.ast.Feature {
+data class Containment(val name: String, val type: Entity, val multiple: Boolean) : me.tomassetti.antlrplus.ast.Feature {
     override fun name() = name
     override fun isMultiple() = multiple
 }
 
-data class Reference(val name: String, val type: PtEntity, val multiple: Boolean) : me.tomassetti.antlrplus.ast.Feature {
+data class Reference(val name: String, val type: Entity, val multiple: Boolean) : me.tomassetti.antlrplus.ast.Feature {
     override fun name() = name
     override fun isMultiple() = multiple
 }
 
-data class Entity(val name:String) {
+data class Entity(val name:String, val features:List<Feature>) {
+
+}
+
+data class Metamodel(val entities: MutableList<Entity> = LinkedList<Entity>()) {
+    fun  hasEntity(name: String) : Boolean = entities.any { it.name == name }
+
+    fun  addEntity(entity: Entity) : Unit {
+        entities.add(entity)
+    }
+
+    fun  byName(name: String): Entity = entities.find { it.name == name } ?: throw IllegalArgumentException("Unknown entity $name")
 
 }
---
+++ src/main/kotlin/me/tomassetti/antlrplus/ast/Processing.kt
@@ -0,0 +1,39 @@
+package me.tomassetti.antlrplus.ast
+
+import me.tomassetti.antlrplus.parsetree.PtEntity
+import me.tomassetti.antlrplus.parsetree.PtFeature
+import me.tomassetti.antlrplus.parsetree.PtMetamodel
+import me.tomassetti.antlrplus.parsetree.TOKEN_TYPE
+
+/**
+ * Map the PtMetamodel to the AST Metamodel and the PtElements to the AST Elements
+ */
+class Mapper {
+
+    fun toAstMetamodel(ptMetamodel: PtMetamodel) : Metamodel {
+        val metamodel = Metamodel()
+        ptMetamodel.entities.forEach { toAstEntity(ptMetamodel, metamodel, it) }
+        return metamodel
+    }
+
+    private fun toAstEntity(ptMetamodel: PtMetamodel, metamodel: Metamodel, ptEntity: PtEntity) : Entity {
+        if (!metamodel.hasEntity(ptEntity.name)) {
+            metamodel.addEntity(Entity(ptEntity.name, ptEntity.features.map { toAstFeature(ptMetamodel, metamodel, it) }))
+        }
+        return metamodel.byName(ptEntity.name)
+    }
+
+    private fun toAstFeature(ptMetamodel: PtMetamodel, metamodel: Metamodel, feature: PtFeature) : Feature {
+        when (feature.type) {
+            TOKEN_TYPE -> return Property(feature.name, Datatype.STRING, feature.multiple)
+            else -> return Containment(feature.name, toAstEntity(ptMetamodel, metamodel, ptMetamodel.byName(feature.name)), feature.multiple)
+        }
+    }
+}
+
+/**
+ * Transform the AST metamodel
+ */
+class Transformer {
+
+}
